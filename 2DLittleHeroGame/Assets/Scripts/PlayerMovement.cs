d59f325eed17d715a016c8e1e547363d53a483ae
Miles Debinski <miles.debinski@gmail.com>
2021-12-20 22:39:48 +0000

    add basic character movement

---
+++ 2DLittleHeroGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+  Vector2 moveInput;
+  Rigidbody2D myRigidbody;
+  [SerializeField] float runSpeed = 5f;
+
+  void Start()
+  {
+    myRigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  void Update()
+  {
+    Run();
+    FlipSprite();
+  }
+
+
+
+  void OnMove(InputValue value)
+  {
+    moveInput = value.Get<Vector2>();
+
+  }
+
+  void Run()
+  {
+    Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+    myRigidbody.velocity = playerVelocity;
+
+  }
+
+  void FlipSprite()
+  {
+    bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+    if (playerHasHorizontalSpeed)
+    {
+      transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+    }
+  }
+}
