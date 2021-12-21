1fac21152e856608a8d00c5de537ba59fe4d094f
Miles Debinski <miles.debinski@gmail.com>
2021-12-21 23:17:06 +0000

    add enemy & mushroom

---
+++ 2DLittleHeroGame/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+  [SerializeField] float moveSpeed = 1f;
+  Rigidbody2D myRiggedbody;
+  void Start()
+  {
+    myRiggedbody = GetComponent<Rigidbody2D>();
+
+  }
+
+  void Update()
+  {
+    myRiggedbody.velocity = new Vector2(moveSpeed, 0);
+  }
+
+  void OnTriggerExit2D(Collider2D other)
+  {
+    moveSpeed = -moveSpeed;
+    FlipEnemyFace();
+  }
+
+  void FlipEnemyFace()
+  {
+    transform.localScale = new Vector2(-transform.localScale.x, 1f);
+
+  }
+
+
+
+}
--- 2DLittleHeroGame/Assets/Scripts/PlayerMovement.cs
+++ 2DLittleHeroGame/Assets/Scripts/PlayerMovement.cs
@@ -7,33 +7,51 @@ public class PlayerMovement : MonoBehaviour
 {
   [SerializeField] float runSpeed = 5f;
   [SerializeField] float jumpSpeed = 14f;
+  [SerializeField] float climbSpeed = 5f;
 
   Vector2 moveInput;
   Rigidbody2D myRigidbody;
   Animator myAnimator;
+  CapsuleCollider2D[] myCapsuleColliders;
+  float gravityScaleAtStart;
+  bool isAlive = true;
+  PlayerInput playerInput;
 
   void Start()
   {
     myRigidbody = GetComponent<Rigidbody2D>();
     myAnimator = GetComponent<Animator>();
+    myCapsuleColliders = GetComponents<CapsuleCollider2D>();
+    gravityScaleAtStart = myRigidbody.gravityScale;
+    playerInput = GetComponent<PlayerInput>();
   }
 
   void Update()
   {
+    if (!isAlive) { return; }
     Run();
+    ClimbLadder();
     FlipSprite();
+    Die();
+
   }
 
   void OnMove(InputValue value)
   {
+    if (!isAlive) { return; }
     moveInput = value.Get<Vector2>();
   }
 
   void OnJump(InputValue value)
   {
+    if (!isAlive) { return; }
+    if (!myCapsuleColliders[1].IsTouchingLayers(LayerMask.GetMask("Ground")))
+    {
+      return;
+    }
+
     if (value.isPressed)
     {
-      // do stuff
       myRigidbody.velocity += new Vector2(0f, jumpSpeed);
     }
   }
@@ -47,6 +65,23 @@ public class PlayerMovement : MonoBehaviour
     myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
   }
 
+  void ClimbLadder()
+  {
+    if (!myCapsuleColliders[0].IsTouchingLayers(LayerMask.GetMask("Ladders")))
+    {
+      myRigidbody.gravityScale = gravityScaleAtStart;
+      myAnimator.SetBool("isClimbing", false);
+      return;
+    }
+
+    Vector2 climbrVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+    myRigidbody.velocity = climbrVelocity;
+    myRigidbody.gravityScale = 0f;
+    bool playerVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+
+    myAnimator.SetBool("isClimbing", playerVerticalSpeed);
+  }
+
   void FlipSprite()
   {
     bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
@@ -55,6 +90,22 @@ public class PlayerMovement : MonoBehaviour
     {
       transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
     }
+  }
 
+  // void onEnemyEncounter()
+  // {
+  //   if (myCapsuleColliders[0].IsTouchingLayers(LayerMask.GetMask("Enemies")))
+  //   {
+  //     playerInput.DeactivateInput();
+  //     Debug.Log("Enemy!!!!");
+  //   }
+  // }
+
+  void Die()
+  {
+    if (myCapsuleColliders[0].IsTouchingLayers(LayerMask.GetMask("Enemies")))
+    {
+      isAlive = false;
+    }
   }
 }
